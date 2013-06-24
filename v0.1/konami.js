363f30f2433b447610f680f7a4d4f3f572b8589d
C. Watson <christopher@keithwatson.com>
2013-06-24 10:24:08 -0500

    Nothing to see here. Move along.
    Signed-off-by: C. Watson <christopher@keithwatson.com>

--- v0.1/konami.js
+++ v0.1/konami.js
@@ -3,19 +3,35 @@
 
 
 
-window.document.onkeyup = func;
+// event listening...
+document.addEventListener('keyup', konami_collect);
 
-var a = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+// ordered key codes that represent the Konami Code
+// UP UP DOWN DOWN LEFT RIGHT LEFT RIGHT B A
+var a = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+// index of progress of Konami Code entry
 var i = 0;
 
-// function to collect the key presses:
-function func(e) {
+// function to collect key presses for the Konami Code:
+function konami_collect(e) {
+	// pressing the correct key updates progress
+	// an incorrect key resets progress
 	e.keyCode == a[i] ? i++ : i = 0;
-	i == 11 ? setInterval("rave()", 25) : null;
+	
+	// upon completion of Konami Code entry, do something
+	i == 10 && konami_execute();
+}
+
+// function run when the Konami Code is entered:
+function konami_execute() {
+	setInterval(rave, 25);
 }
 
 // function to do crazy shit:
 function rave() {
-	var color = "#" + Math.floor(Math.random() * 16777216).toString(16);
+	// color defined with HSL rather than RGB so that "intensity" (or whatever
+	// the appropriate term is) appears more consistant
+	var color = 'hsl(' + Math.floor(Math.random() * 360) + ', 100%, 50%)';
 	document.body.style.backgroundColor = color;
 }
